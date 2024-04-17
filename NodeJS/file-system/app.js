b9ea1ba429c3615ddd51c3bfa0ac01f86a397edc
MccXaccess <115007251+MccXaccess@users.noreply.github.com>
2024-04-17 18:55:52 +0500

    Command execution program

--- NodeJS/file-system/app.js
+++ NodeJS/file-system/app.js
@@ -7,6 +7,97 @@ const fs = require("fs/promises");
 
 
 (async () => {
+    // commands 
+    const CREATE_FILE = 'create a file';
+    const DELETE_FILE = 'delete a file';
+    const RENAME_FILE = 'rename the file';
+    const ADD_TO_FILE = 'add to file';
+
+    const createFile = async (path) => {
+        try {
+            // we want to check whether or not we already have that file
+            const existingFileHandle = await fs.open(path, "r");
+            existingFileHandle.close();
+
+            // we already have that file...
+            return console.log(`The file ${path} already exists.`);
+        } catch (e) {
+            // we don't have the file, now we should create it
+            const newFileHandle = await fs.open(path, "w");
+            console.log("A new file was successfully created.");
+            newFileHandle.close();
+        }
+    };
+    
+    const deleteFile = async (path) => {
+        try {
+            await fs.rm(path);
+            console.log('REMOVE RESULTS');
+        }
+        catch (e) 
+        {
+            
+        }
+    }
+
+    const renameFile = async (oldPath, newPath) => {
+        try {
+            const result = await fs.rename(oldPath, newPath);
+            console.log('RENAME RESULTS', result);
+        }
+        catch (e) 
+        {
+            console.log('SOMETHING WENT WRONG', e);
+        }
+    }
+
+    const addToFile = async (filePath, content) => {
+        try {
+            // we want to check whether or not we already have that file
+            const existingFileHandle = await fs.open(filePath, "w");
+             
+            existingFileHandle.write(content, 0, 'utf-8');
+
+            existingFileHandle.close();
+
+
+            // we already have that file...
+            return console.log(`ENTRY ADDED`);
+        }
+        catch (e) 
+        {
+        }
+    }
+
+    const conditions = (command) => {
+        // create a file:
+        // create a file <path> 
+        if (command.includes(CREATE_FILE)) {
+            const filePath = command.substring(CREATE_FILE.length + 1);
+            createFile(filePath);
+        }
+        // delete a file <path> 
+        if (command.includes(DELETE_FILE)) {
+            const filePath = command.substring(DELETE_FILE.length + 1);
+            deleteFile(filePath);
+        }
+        // rename a file <path> 
+        if (command.includes(RENAME_FILE)) {
+            const overall = command.split(' ');
+            // const oldPath = command.substring(RENAME_FILE.length + 1);
+            
+            renameFile(overall[3], overall[4]);
+        }
+        // add to file <path> 
+        if (command.includes(ADD_TO_FILE)) {
+            const overall = command.split(' ');
+
+            // const filePath = command.substring(ADD_TO_FILE.length + 1);
+            
+            addToFile(overall[3], overall[4]);
+        }
+    }
+
     // cuz it returns a promise we use await.
     const commandFileHandler = await fs.open('./commands.txt', 'r');
 
@@ -25,7 +116,14 @@ const fs = require("fs/promises");
 
             // returns a Buffer, huge buffer allocated, otherwise
             console.log(content);
+
+            // AS WE FILLED OUR DATA TO BUFFER WE PROVIDED, NO NEED TO SAVE IT TO CONTENT VAR
+            // WE CAN PRINT DIRECTLY THE BUFFER VALUE DECODED
+            console.log(buffer.toString('utf-8'));
         
+            const command = buffer.toString('utf-8');
+
+            conditions(command);
     })
 
     // watching whole directory, can change to commands.txt only
